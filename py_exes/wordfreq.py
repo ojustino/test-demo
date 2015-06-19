51542649d4cb5c803b8d52032147432380de7de5
Oderah Otor <ootor@logos.astro.Princeton.EDU>
2015-06-19 10:42:05 -0400

    changes to wordfreq

--- py_exes/wordfreq.py
+++ py_exes/wordfreq.py
@@ -4,8 +4,42 @@ def wordfreq(filename):
     f1 = open(filename)
     xx1 = f1.readlines()
 
-    speech = 
-# make some dictionary
+    speech = {}
+
+    # create dictionary of relevant words (longer than 3 characters)
+    for row in xx1: 
+        row = row.split()
+        for word in row:
+            if word.isalpha(): #if word is totally alphabetic
+                word = word.lower()
+                if word in speech and len(word) > 3:
+                    speech[word] += 1 # increment it
+                if word not in speech and len(word) > 3:
+                    speech[word] = 1  # add it to the dict
+                else:
+                    pass
+                continue
+            if word[0:-1].isalpha(): # if word is alpha but has punct at end
+                word = word[0:-1].lower()
+                if word in speech and len(word) > 4:
+                    speech[word] += 1 # increment it
+                if word not in speech and len(word) > 4:
+                    speech[word] = 1  # add it to the dict
+                else:
+                    pass
+                continue
+            else:
+                pass
+    
+    # now print sorted style
+    #print sorted(speech, key=lambda x: x[1])
+            
+    print speech
+
+wordfreq('ihaveadream.txt')
+
+
+        
 # make all words lowercase, add together words that were previously in different cases
 # discard all punctuation and numbers
 # x=
